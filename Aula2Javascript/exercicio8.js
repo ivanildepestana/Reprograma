6d86710ccf7a16f311177de4ecc136be1c703c4c
Ivanilde Mota Pestana <ivanilde.pestana@FEDUC.BR>
2016-10-26 11:27:29 -0200

    tentando subir arquivo

--- Aula2Javascript/exercicio8.js
+++ Aula2Javascript/exercicio8.js
@@ -1,11 +1,11 @@
-var nome=prompt("*Qual é o seu nome?:");
-var sexo=prompt("Qual é o seu sexo?:");
+var nome = prompt("Qual é o seu nome?:, *Preenchimento obrigatório")
+var sexo = prompt("Qual é o seu sexo?:")
  
 
-if (sexo == "feminino");{
-	alert("Bem Vinda," + nome);
-} else if (sexo == "masculino");
-	alert("Bem Vindo," + nome);
-} else  (sexo == ""); {
+if ( sexo == "feminino"){
+	alert("Bem Vinda," + nome)
+} else if (sexo == "masculino"){
+	alert("Bem Vindo," + nome)
+} else if ( sexo == "") {
 	alert("Olá!," + nome)
 };
